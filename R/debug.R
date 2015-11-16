6daef4fbb55025abf2484d43ad4e84a6a92ddc76
Erik Loualiche <erikl@mit.edu>
2015-11-16 17:58:35 -0500

    - Country business pattern

--- R/debug.R
+++ R/debug.R
@@ -1,1 +1,20 @@
-dt_res <- download_all_cbp()
+# dt_res <- download_all_cbp()
+#
+# dt_tmp <- dt_res[, list(year, fipscty, naics) ]
+# dt_tmp <- dt_tmp[ !is.na(naics) ]
+# dt_tmp[, `:=`(naics3      = str_sub(naics, 1, 3),
+#               naics3_type = str_sub(naics, 4, 6) ) ]
+#
+# dt_tmp[ naics3_type == "---" | naics3_type == "///" ]
+# dt_tmp[ naics3_type == "---", list(naics, naics, naics3_type) ] %>% unique
+# dt_tmp[ naics3_type == "///", list(naics, naics, naics3_type) ] %>% unique
+#
+#
+# library(stringr)
+#
+# year <- 2000
+# str_sub(year, 3, 4)
+#
+# s2000 %% 100
+#
+# dt <- fread("~/Downloads/naics.txt", header = T, sep="  ")
--- R/get_cbp_data.R
+++ R/get_cbp_data.R
@@ -1,9 +1,40 @@
-#' Group all the functions
+# Group all the functions
+
+
+
+#' Download the CBP
 #'
 #' @note downloads data from the County Business Pattern
 #'   for desired years, splitting data by the aggregation level codes
 #'   the docs for the CBP is available at the following:
 #'   http://www.census.gov/econ/cbp/download/full_layout/County_Layout_SIC.txt
+#' @param year_start: year starting data download
+#' @param year_end: year ending data download
+#' @param path_data: where does the download happen: default current directory
+#' @return data.table
+#' @export
+download_all_cbp <- function(
+  year_start = 1996,
+  year_end   = 2013,
+  path_data = "~/Downloads/"
+){
+
+  dt_res <- data.table()
+
+  for (year in seq(year_start, year_end)){
+
+    message(paste0("Downloading and processing year: ", year))
+    dt_year <- download_cbp_data(target_year = year, path_data = path_data)
+    dt_year[, year := year ]
+    dt_res  <- rbind(dt_res, dt_year, fill = T)
+
+  }
+
+  return(dt_res)
+
+}
+
+
 
 #' Download one year of county business pattern dataset
 #'
@@ -41,29 +72,7 @@ download_cbp_data <- function(
 
 }
 
-#' Download the CBP
-#'
-#' @param target_year: year for which we want to download the data
-#' @param path_data: where does the download happen: default current directory
-#' @return data.table
-#' @export
-download_all_cbp <- function(
-  year_start = 1996,
-  year_end   = 2013,
-  path_data = "~/Downloads/"
-){
 
-    dt_res <- data.table()
 
-    for (year in seq(year_start, year_end)){
 
-      message(paste0("Downloading and processing year: ", year))
-      dt_year <- download_cbp_data(target_year = year, path_data = path_data)
-      dt_year[, year := year ]
-      dt_res  <- rbind(dt_res, dt_year, fill = T)
 
-    }
-
-  return(dt_res)
-
-}
