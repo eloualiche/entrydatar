06ff0904af7dd8454b0174e8c3b4bc0351b600f1
Erik Loualiche <erikloulou@gmail.com>
2018-03-13 19:26:45 -0400

    New interface to the full flat file of the BES through get_bed_detail. See details in vignette

--- R/get_bed_data.R
+++ R/get_bed_data.R
@@ -10,9 +10,9 @@
 #'
 #' @param which_data: what kind of data download: default is the main industry file
 #' @param path_data: where does the download happen: default current directory
-#' @return df_res.
+#' @return df
 #' @export
-get_bed_data = function(
+get_bed = function(
   which_data = "industry",
   path_data = "./"
 ){
@@ -84,3 +84,109 @@ get_bed_data = function(
 } # end of download_data
 
 
+
+
+#' Download BED dataset from directly from the BLS website
+#'
+#' @param seasonaladj: seasonality adjustment of the data; S (default) or U
+#' @param msa: msa code; 00000 (default) for national
+#' @param state: state code; 00 (default) for national
+#' @param county: county code; 000 (default) for national
+#' @param industry: industry code; 000000 (default) for total private. See docs for industry classifications
+#' @param unitanalysis: unit of observation; 1 (default) for establishment
+#' @param dataelement: which type of date; 2 (default) number of establishments, 1 employment
+#' @param sizeclass: firm size class; 00 (default) for all
+#' @param dataclass: for type of information; 07 (default) for Establishment Births, 01 Gross Job Gains, 02 Expansions, 03 Openings, 04 Gross Job Losses, 05 Contractions, 06 Closings, 07 Establishment Births, 08 Establishment Deaths
+#' @param ratelevel: for type of statistics; L (default) for level, R for rate
+#' @param periodicity: for sampling frequency; Q (default) for quarterly, A for annual
+#' @param ownership: for type of ownership; 5 (default) for private sector
+#' @param download: requires download confirmation
+#' @param read
+#' @param path_data: where does the download happen: default current directory
+#' @return dt_final
+#' @export
+get_bed_detail <- function(
+  seasonaladj  = "S",
+  msa          = "00000",
+  state        = "00",
+  county       = "000",
+  industry     = "000000", # Total Private
+  unitanalysis = 1, # Establishment
+  dataelement  = 2,# `1` Employment, `2` Number of Establishments
+  sizeclass    = "00",
+  dataclass    = c("07"),
+  ratelevel    = "L", # `L` Level, `R` Rate
+  periodicity  = "Q", # `A` Annual, `Q` Quarterly
+  ownership    = 5,   #  `5` Private Sector
+  download     = F,
+  read         = F,
+  path_data    = "./"
+){
+
+  # Check if download is required
+  d_choice <- TRUE
+  if (download != TRUE){
+    if (read == FALSE){
+      d_choice = as.logical(
+        utils::menu(c("No", "Yes"),
+                    title = "Are you sure you want do download the BED flat file, size 225Mb?") - 1
+      )
+    } else if (read == TRUE){
+      dt_all    <- readRDS(paste0(path_data, "bed_all.rds"))
+      dt_lookup <- readRDS(paste0(path_data, "bed_lookup.rds"))
+    }
+  }
+
+  # Download the flat file and the lookup table
+  if (read == FALSE){
+    if (download == TRUE | d_choice == TRUE){
+      message("# Downloading files ...")
+      message("# Download full flat file ...")
+      download.file("https://download.bls.gov/pub/time.series/bd/bd.data.1.AllItems",
+                    paste0(path_data, "bed_all.txt") )
+      message("# Download lookup table ...")
+      download.file("https://download.bls.gov/pub/time.series/bd/bd.series",
+                    paste0(path_data, "bed_lookup.txt") )
+    } else {
+      stop("Could not download the flat BED file")
+    }
+
+    # read the file
+    message("# Reading downloaded files ...")
+    dt_all <- fread(paste0(path_data, "bed_all.txt"))
+    dt_lookup <- fread(paste0(path_data, "bed_lookup.txt"), colClasses = rep("character", 19) )
+  }
+
+
+  dt_lookup <-
+    dt_lookup[   seasonal          %in% seasonaladj  &
+                 msa_code          %in% msa          &
+                 state_code        %in% state        &
+                 county_code       %in% county       &
+                 industry_code     %in% industry     &
+                 unitanalysis_code %in% unitanalysis &
+                 dataelement_code  %in% dataelement  &
+                 sizeclass_code    %in%   sizeclass  &
+                 dataclass_code    %in% dataclass    &
+                 ratelevel_code    %in%  ratelevel   &
+                 periodicity_code  %in%  periodicity &
+                 ownership_code    %in% ownership,
+               .(series_id, series_title) ]
+
+  dt_final <- dt_all[ series_id %in% dt_lookup$series_id ]
+  dt_final[, date := as.Date(ISOdate(year, as.numeric(substr(period,2,3))*3, 1)) ]
+  dt_final[, date_ym := year(date)*100+month(date) ]
+  dt_final[, value := as.numeric(value) ]
+  dt_final[, c("year", "period", "footnote_codes") := NULL ]
+  dt_final <- merge(dt_final, dt_lookup, all.x = T, by = c("series_id"))
+
+    # cleaning up
+  message("# Cleaning downloaded files ... \n")
+    file.remove( paste0(path_data, "bed_all.txt" ) )
+    file.remove( paste0(path_data, "bed_lookup.txt" ) )
+
+
+    # return dataset
+    return( dt_final )
+
+} # end of download_data
--- vignettes/bed.md
+++ vignettes/bed.md
@@ -10,6 +10,29 @@ dt_ind <- get_bed_data("industry")
 dt_ind
 ```
 
+Take for example industry `naics = 111`, *Crop Production*. The data comes from two tables:
+
++ [Table 7](https://www.bls.gov/web/cewbd/table7_1_ind3.txt)
+  + *Private sector establishments by direction of employment change, as percent of total establishments, seasonally adjusted*
+  + `ent_cnt` is *Establishment gaining jobs, Opening establishments*
+  + `exit_cnt` is *Establishment losing jobs, Closing establishments*
+  + `nent_cnt` is the difference between the number of opening establishments and the number of closing establishments. 
+  
++ [Table 3](https://www.bls.gov/web/cewbd/table3_1_ind3.txt)
+  + *Private sector gross job gains and losses, as a percent of employment, seasonally adjusted*
+  + `ent_emp` is *Gross job gains, Opening establishments*
+  + `exit_emp` is *Gross job losses, Closing establishments*
+  + `nent_emp` Net change is the difference between total gross job gains and total gross job losses.
+
+
+# In progress
+
+# Other informations
+Business Employment Dynamics Program Contacts
+Please contact us via email at: BDMInfo@bls.gov or by phone on 202-691-6553.
+
+
+
 ------------------------------------------------------------------------
 
 1.  Erik Loualiche
