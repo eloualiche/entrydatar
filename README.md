a1f5565733bd3c40d4bfe19db46950f6e729e070
Erik Loualiche <erikloulou@gmail.com>
2018-03-30 19:18:04 -0500

    remove DT

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 entrydatar: download and clean data on U.S. firms' entry
 ======
 
+[![Build Status](https://travis-ci.org/eloualiche/entrydatar.svg?branch=master)](https://travis-ci.org/eloualiche/entrydatar)
+
+
+
 Downloads and aggregates flat tables of entry statistics from the *Quarterly Census of Employment and Wages* and *Business Employment Dynamics program* and the *County Business Patterns*. See details of this file and the vignettes for other commands for other public downloads.
 
 ## Available data
--- tests/testthat/test-bed.R
+++ tests/testthat/test-bed.R
@@ -3,15 +3,15 @@ context("Download the BED data")
 test_that("Get the BED and check variables", {
   skip_on_cran()
 
-dt_ind <- get_bed("industry")
+  dt_ind <- get_bed("industry")
 
   # dataset structure
   expect_equal(colnames(dt_ind),
-               c("date_ym", "naics3", "year", "period", "ent_emp", "note1", 
+               c("date_ym", "naics3", "year", "period", "ent_emp", "note1",
                  "exit_emp", "ent_cnt", "exit_cnt", "nent_emp", "nent_cnt"))
 
   # first row
   expect_equal(min(dt_ind$date_ym), 199209)
-  
-  
+
+
 })
